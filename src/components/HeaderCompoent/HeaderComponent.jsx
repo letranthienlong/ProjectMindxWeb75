6fbe6dac3685e6f2ae6518e2e1785ba35ab3fbaf
letranthienlong <thienlong19095@gmail.com>
2024-03-25 20:01:48 +0700

    update admin page

--- src/components/HeaderCompoent/HeaderComponent.jsx
+++ src/components/HeaderCompoent/HeaderComponent.jsx
@@ -13,55 +13,61 @@ import * as UserService from '../../services/UserService'
 import { resetUser } from '../../redux/slides/userSlide';
 import Loading from '../LoadingComponent/Loading';
 
-const HeaderComponent = () => {
+const HeaderComponent = ({ isHiddenSearch = false, isHiddenCart = false }) => {
   const navigate = useNavigate()
   const user = useSelector((state) => state.user)
   const dispatch = useDispatch()
   const [userName, setUserName] = useState('')
   const [userAvatar, setUserAvatar] = useState('')
-  const [loading, setLoading] = useState(false)
+  const [pending, setPending] = useState(false)
   const handleNavigateLogin = () => {
     navigate('/sign-in')
   }
 
   const handleLogout = async () => {
-    setLoading(true)
+    setPending(true)
     await UserService.logoutUser()
     dispatch(resetUser())
-    setLoading(false)
+    setPending(false)
   }
 
   useEffect(() => {
-    setLoading(true)
+    setPending(true)
     setUserName(user?.name)
     setUserAvatar(user?.avatar)
-    setLoading(false)
+    setPending(false)
   }, [user?.name, user?.avatar])
 
   const content = (
     <div>
-      <WrapperContentPopup onClick={handleLogout}>Đăng xuất</WrapperContentPopup>
       <WrapperContentPopup onClick={() => navigate('/profile-user')}>Thông tin người dùng</WrapperContentPopup>
+      {user?.isAdmin && (
+
+        <WrapperContentPopup onClick={() => navigate('/system/admin')}>Quản lí hệ thống</WrapperContentPopup>
+      )}
+      <WrapperContentPopup onClick={handleLogout}>Đăng xuất</WrapperContentPopup>
     </div>
   );
 
   return (
     <div style={{ width: '100%', background: 'rgb(26, 148, 255)', display: 'flex', justifyContent: 'center' }}>
-      <WrapperHeader>
+      <WrapperHeader style={{ justifyContent: isHiddenSearch && isHiddenSearch ? 'space-between' : 'unset' }}>
         <Col span={5}>
           <WrapperTextHeader>Nhóm Long Trường Kiệt</WrapperTextHeader>
         </Col>
-        <Col span={13}>
-          <ButttonInputSearch
-            size="large"
-            bordered={false}
-            textButton="Tìm kiếm"
-            placeholder="input search text"
-          // onSearch={onSearch}
-          />
-        </Col>
+        {!isHiddenSearch && (
+          <Col span={13}>
+            <ButttonInputSearch
+              size="large"
+              bordered={false}
+              textButton="Tìm kiếm"
+              placeholder="input search text"
+            // onSearch={onSearch}
+            />
+          </Col>
+        )}
         <Col span={6} style={{ display: 'flex', gap: '54px', alignItems: 'center' }}>
-          <Loading isPending={loading}>
+          <Loading isPending={pending}>
             <WrapperHeaderAccout>
               {userAvatar ? (
                 <img src={userAvatar} alt="avatar" style={{
@@ -90,12 +96,14 @@ const HeaderComponent = () => {
               )}
             </WrapperHeaderAccout>
           </Loading>
-          <div>
-            <Badge count={4} size="small">
-              <ShoppingCartOutlined style={{ fontSize: '30px', color: '#fff' }} />
-            </Badge>
-            <WrapperTextHeaderSmall>Giỏ hàng</WrapperTextHeaderSmall>
-          </div>
+          {!isHiddenCart && (
+            <div>
+              <Badge count={4} size="small">
+                <ShoppingCartOutlined style={{ fontSize: '30px', color: '#fff' }} />
+              </Badge>
+              <WrapperTextHeaderSmall>Giỏ hàng</WrapperTextHeaderSmall>
+            </div>
+          )}
         </Col>
       </WrapperHeader>
     </div>
